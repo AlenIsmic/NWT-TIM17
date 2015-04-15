fdeb74782f09d221492685a6adb151b129c2ec96
merimah <merimah.mh@gmail.com>
2015-04-15 18:55:25 +0200

    updated db script
    updated db script

--- bookStoreDatabaseScript.sql
+++ bookStoreDatabaseScript.sql
@@ -1,161 +1,194 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2015 at 10:29 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema book_store
--- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema book_store
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `book_store` DEFAULT CHARACTER SET latin1 ;
-USE `book_store` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_author`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_author` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL,
-  `biography` VARCHAR(50) NOT NULL,
+--
+-- Database: `book_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_author`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `biography` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`biography` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`biography`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `tbl_author`
+--
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_book`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_book` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(50) NOT NULL,
-  `genre` VARCHAR(50) NULL DEFAULT NULL,
-  `cover` VARCHAR(100) NULL DEFAULT NULL,
-  `summary` TEXT NULL DEFAULT NULL,
-  `price` DOUBLE NOT NULL,
-  `available` INT(11) NULL DEFAULT NULL,
+INSERT INTO `tbl_author` (`id`, `name`, `biography`) VALUES
+(1, 'Agatha Christie', 'She is famous crimi writer.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_book`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `cover` varchar(100) DEFAULT NULL,
+  `summary` text,
+  `price` double NOT NULL,
+  `available` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
+--
+-- Dumping data for table `tbl_book`
+--
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_book_author`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_book_author` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `author` INT(11) NOT NULL,
-  `book` INT(11) NOT NULL,
+INSERT INTO `tbl_book` (`id`, `title`, `genre`, `cover`, `summary`, `price`, `available`) VALUES
+(1, '', NULL, 'Book', 'Book is great!', 9.99, NULL),
+(2, '', NULL, '', '', 0, NULL),
+(3, '', NULL, 'cover', 'summary', 9.98, NULL),
+(4, '', NULL, 'cover', 'summary', 9.98, NULL),
+(5, '', NULL, 'cover', 'summary', 9.98, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_book_author`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_book_author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` int(11) NOT NULL,
+  `book` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC),
-  INDEX `author_idx` (`author` ASC),
-  INDEX `book` (`book` ASC),
-  CONSTRAINT `author`
-    FOREIGN KEY (`author`)
-    REFERENCES `book_store`.`tbl_author` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `book`
-    FOREIGN KEY (`book`)
-    REFERENCES `book_store`.`tbl_book` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`),
+  KEY `author_idx` (`author`),
+  KEY `book` (`book`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_user` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
-  `admin` INT(11) NULL DEFAULT NULL,
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `user` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `email_UNIQUE` (`user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_order` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NOT NULL,
-  `user` INT(11) NOT NULL,
-  `price` INT(11) NOT NULL,
+--
+-- Table structure for table `tbl_review`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(150) NOT NULL,
+  `user` int(11) NOT NULL,
+  `rate` int(11) DEFAULT NULL,
+  `type` varchar(45) NOT NULL,
+  `reference` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`user` ASC),
-  CONSTRAINT `user`
-    FOREIGN KEY (`user`)
-    REFERENCES `book_store`.`tbl_user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_review`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_review` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `content` VARCHAR(150) NOT NULL,
-  `user` INT(11) NOT NULL,
-  `rate` INT(11) NULL DEFAULT NULL,
-  `type` VARCHAR(45) NOT NULL,
-  `reference` INT(11) NOT NULL,
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `admin` int(11) DEFAULT NULL,
+  `isVerified` tinyint(1) NOT NULL,
+  `password` varchar(30) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `tbl_user`
+--
 
--- -----------------------------------------------------
--- Table `book_store`.`tbl_user_book`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store`.`tbl_user_book` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `user` INT(11) NOT NULL,
-  `book` INT(11) NOT NULL,
-  `state` VARCHAR(50) NOT NULL,
-  `order` INT(11) NULL DEFAULT NULL,
+INSERT INTO `tbl_user` (`id`, `name`, `email`, `admin`, `isVerified`, `password`) VALUES
+(1, 'ime', 'mail', NULL, 0, 'psw'),
+(2, '', '', NULL, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_book`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `book` int(11) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_id` (`id` ASC),
-  INDEX `order_idx` (`order` ASC),
-  INDEX `user2` (`user` ASC),
-  INDEX `book2` (`book` ASC),
-  CONSTRAINT `user2`
-    FOREIGN KEY (`user`)
-    REFERENCES `book_store`.`tbl_user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `book2`
-    FOREIGN KEY (`book`)
-    REFERENCES `book_store`.`tbl_book` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `order`
-    FOREIGN KEY (`order`)
-    REFERENCES `book_store`.`tbl_order` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `unique_id` (`id`),
+  KEY `order_idx` (`order`),
+  KEY `user2` (`user`),
+  KEY `book2` (`book`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Constraints for dumped tables
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Constraints for table `tbl_book_author`
+--
+ALTER TABLE `tbl_book_author`
+  ADD CONSTRAINT `author` FOREIGN KEY (`author`) REFERENCES `tbl_author` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `book` FOREIGN KEY (`book`) REFERENCES `tbl_book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD CONSTRAINT `user` FOREIGN KEY (`user`) REFERENCES `tbl_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_user_book`
+--
+ALTER TABLE `tbl_user_book`
+  ADD CONSTRAINT `user2` FOREIGN KEY (`user`) REFERENCES `tbl_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `book2` FOREIGN KEY (`book`) REFERENCES `tbl_book` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `order` FOREIGN KEY (`order`) REFERENCES `tbl_order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
